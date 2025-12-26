a9329e8c6b88b618927158d0b94b02033eb35aa1
Stepan Vasilyev <intellectualspecialist@gmail.com>
2025-12-26 17:20:05 +0500

    refactor: валидация выделена в отдельный модуль

--- js/form.js
+++ js/form.js
@@ -1,60 +1,11 @@
 import { isEscKey } from './utils.js';
+import { initFormValidation } from './form-validation.js';
 
 const bodyElement = document.querySelector('body');
 const formElemet = bodyElement.querySelector('.img-upload__form');
 const modalFormElement = formElemet.querySelector('.img-upload__overlay');
 const uploadControlElement = formElemet.querySelector('.img-upload__input');
 const modalFormCloseElement = modalFormElement.querySelector('.img-upload__cancel');
-const hashtagsFieldElement = modalFormElement.querySelector('.text__hashtags');
-const descriptionFieldElement = modalFormElement.querySelector('.text__description');
-
-const pristine = new Pristine(formElemet, {
-  classTo: 'img-upload__field-wrapper', // Элемент, на который будут добавляться классы
-  errorTextParent: 'img-upload__field-wrapper', // Элемент, куда будет выводиться текст с ошибкой
-  errorTextTag: 'div', // Тег, который будет обрамлять текст ошибки
-  errorTextClass: 'img-upload__field-wrapper--error' // Класс для элемента с текстом ошибки
-});
-const hashtag = /^#[a-zа-яё0-9]{1,19}$/i;
-
-const validateHashtag = (value) => hashtag.test(value);
-
-const validateHashtagsField = (value) => {
-  if (!value) {
-    return true;
-  }
-
-  const hashtags = value.trim().split(/\s+/);
-
-  return hashtags.every((item) => validateHashtag(item)) && hashtags.length <= 5 && new Set(hashtags).size === hashtags.length;
-};
-
-const validateDescriptionField = (value) => value.length <= 140;
-
-const getHashtagsErrorMessage = (value) => {
-  const hashtags = value.trim().split(/\s+/);
-
-  if (!hashtags.every((item) => validateHashtag(item))) {
-    return 'Невалидный хэштег';
-  }
-
-  if (new Set(hashtags).size !== hashtags.length) {
-    return 'Хэштеги повторяются';
-  }
-
-  if (hashtags.length > 5) {
-    return 'Не более 5 хэштегов';
-  }
-};
-
-pristine.addValidator(hashtagsFieldElement, validateHashtagsField, getHashtagsErrorMessage);
-pristine.addValidator(descriptionFieldElement, validateDescriptionField, 'Не больше 140 символов');
-
-formElemet.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  if (pristine.validate()) {
-    formElemet.submit();
-  }
-});
 
 const onDocumentKeydown = (evt) => {
   if (isEscKey(evt)) {
@@ -81,6 +32,10 @@ function closeModalForm() {
 }
 
 const initModalForm = () => {
+  if (!modalFormElement) {
+    return;
+  }
+
   uploadControlElement.addEventListener('change', () => {
     openModalForm();
   });
@@ -88,6 +43,8 @@ const initModalForm = () => {
   modalFormCloseElement.addEventListener('click', () => {
     closeModalForm();
   });
+
+  initFormValidation();
 };
 
 export { initModalForm };
